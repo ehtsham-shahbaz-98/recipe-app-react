3fa54132150933d6e4efc41de4dd5d7918bb05ba
Ehtsham Shahbaz <ehtsham.shahbaz98@gmail.com>
2021-08-24 20:47:41 -0700

    First commit

---
+++ src/component/Basics/MenuApi.js
@@ -0,0 +1,64 @@
+const Menu = [
+  {
+    id: 1,
+    image:
+      "https://images.pexels.com/photos/2087748/pexels-photo-2087748.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Tacos",
+    category: "dinner",
+    price: "$25",
+    description:
+      "Tacos. lorem ipsum dolor sit amet consectetur adispisicing elit. Dolorelibreo cum obcaecati deserunt excepturi. Amet?",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/14737/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Spaghetti",
+    category: "lunch",
+    price: "$30",
+    description:
+      "Spaghetti. lorem ipsum dolor sit amet consectetur adispisicing elit. Dolore libreo cum obcaecati deserunt excepturi. Amet?",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.pexels.com/photos/2323398/pexels-photo-2323398.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Sushi",
+    category: "breakfast",
+    price: "$25",
+    description:
+      "Sushi. lorem ipsum dolor sit amet consectetur adispisicing elit. Dolore libreo cum obcaecati deserunt excepturi. Amet?",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.pexels.com/photos/2347311/pexels-photo-2347311.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Chinese Noodles",
+    category: "breakfast",
+    price: "$25",
+    description:
+      "Chinese Noodles. lorem ipsum dolor sit amet consectetur adispisicing elit. Dolore libreo cum obcaecati deserunt excepturi. Amet?",
+  },
+  {
+    id: 5,
+    image:
+      "https://images.pexels.com/photos/2673353/pexels-photo-2673353.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Roasted Chicken",
+    category: "lunch",
+    price: "$30",
+    description:
+      "Roasted Chicken. lorem ipsum dolor sit amet consectetur adispisicing elit. Dolore libreo cum obcaecati deserunt excepturi. Amet?",
+  },
+  {
+    id: 6,
+    image:
+      "https://images.pexels.com/photos/5637760/pexels-photo-5637760.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Barbecue",
+    category: "evening",
+    price: "$35",
+    description:
+      "Barbecue. lorem ipsum dolor sit amet consectetur adispisicing elit. Dolorelibreo cum obcaecati deserunt excepturi. Amet?",
+  },
+];
+
+export default Menu;
